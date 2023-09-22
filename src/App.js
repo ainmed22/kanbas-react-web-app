ace8af6b87d67cbebc9899f8c38865e9b2255909
ainmed22 <90437574+ainmed22@users.noreply.github.com>
2023-09-22 00:47:51 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,15 +7,15 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Go to index.html.
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="../public/index/html"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Click here!
         </a>
       </header>
     </div>
