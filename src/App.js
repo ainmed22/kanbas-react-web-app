a0653e1803a8ae4ae5b45e85e55ef7c4807216e0
ainmed22 <90437574+ainmed22@users.noreply.github.com>
2023-09-26 14:17:29 -0400

    Revert App.js

--- src/App.js
+++ src/App.js
@@ -7,15 +7,15 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Go to index.html.
+          Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
-          href="../public/index.html"
+          href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Click here!
+          Learn React
         </a>
       </header>
     </div>
