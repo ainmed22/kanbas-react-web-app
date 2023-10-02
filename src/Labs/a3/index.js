67705d907195d800383161d5f899f09a7e57044c
BuildTools <unconfigured@null.spigotmc.org>
2023-10-02 18:27:13 -0400

    Lab 3 WIP 3

--- src/Labs/a3/index.js
+++ src/Labs/a3/index.js
@@ -1,9 +1,17 @@
 import JavaScript from "./JavaScript";
+import Classes from "./Classes";
+import Styles from "./Styles";
+import ConditionalOutput from "./ConditionalOutput";
+import TodoList from "./todo/TodoList";
 
 function Assignment3() {
     return (
         <div className="container">
             <h1>Assignment 3</h1>
+            <TodoList/>
+            <ConditionalOutput/>
+            <Styles/>
+            <Classes/>
             <JavaScript/>
         </div>
     );
