857550ba90dedc85442bd63f6bb5beb9b9fd1841
unknown <medado.a@northeastern.edu>
2023-11-21 02:52:27 -0500

    Assignment 6 reverts

--- src/Kanbas/users/client.js
+++ src/Kanbas/users/client.js
@@ -8,16 +8,12 @@ export const BASE_API = process.env.REACT_APP_API_BASE;
 export const USERS_API = `${BASE_API}/api/users`;
 
 export const signin = async (credentials) => {
-    console.log(`${USERS_API}/signin`);
     const response = await request.post(`${USERS_API}/signin`, credentials);
-    console.log(response.data);
     return response.data;
 };
 
 export const account = async () => {
     const response = await request.post(`${USERS_API}/account`);
-    console.log("account is:");
-    console.log(response.data);
     return response.data;
 };
 
